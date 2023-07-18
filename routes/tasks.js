906c7b6423335e9406f8b3ee155ab8f6111d8efc
JacquelineMG <j.gilchrist@outlook.com>
2023-07-18 08:54:42 -0700

    start routes

---
+++ routes/tasks.js
@@ -0,0 +1,32 @@
+/*
+ * All routes for Users are defined here
+ * Since this file is loaded in server.js into /users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+// Get the list
+router.get('/', (req, res) => {
+
+});
+
+
+// Add new item to list
+router.post('/', (req, res) => {
+
+});
+
+// Edit an item
+router.post('/:id', (req, res) => {
+
+});
+
+//Delete an item
+router.post('/:id/delete', (req, res) => {
+
+});
+
+module.exports = router;
