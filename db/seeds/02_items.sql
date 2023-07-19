b61d7774217e8cea2935c3a39d653aa0308caaec
Carmelia Shito <carmshito@hotmail.com>
2023-07-19 12:31:51 -0400

    add schema and seed files for categories and items

---
+++ db/seeds/02_items.sql
@@ -0,0 +1,8 @@
+INSERT INTO items (category_id, title, is_done)
+VALUES (1, 'Titanic', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (4, 'Shoes', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (2, 'Atomic Habits', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (3, 'Wendys', false);
