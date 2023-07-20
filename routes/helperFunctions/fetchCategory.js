98a5866637763752a9eff01a194046f48b5ac5f6
JacquelineMG <j.gilchrist@outlook.com>
2023-07-20 09:44:33 -0700

    add getCatId function

--- routes/helperFunctions/fetchCategory.js
+++ routes/helperFunctions/fetchCategory.js
@@ -2,116 +2,92 @@
 
 const fetch = require('node-fetch');
 
-const yelp = require('yelp-fusion');
-const client = yelp.client('tBdsUQE8RCcOfHvyQM_4pRHS_xuN0uOzNHwVMyMyLpDHEYqbYYABAvsnno4VOnIG0mcvNuAccWuRUeWF3kLigiJaEp2_R12Zn_EPfVWFnyR1U47NkRGFheq3MyO3ZHYx')
+// OpenAIApi configuration
+// Adapted from https://platform.openai.com/docs/quickstart
 
 
-// helper function
-const { urlEncode } = require('./urlEncode')
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  organization: process.env.OPENAI_ORG_ID,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const apiKey = "Bearer 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";
 
 
-///////////////////////////////////////////////////////////////////////////
-
-const toWatch = async function(input) {
-  const title = urlEncode(input)
-
-  const url = `https://imdb-search2.p.rapidapi.com/${title}`;
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '02f1e4e396mshf4d0b21c733446bp12fe4cjsn159bedb15bbe',
-      'X-RapidAPI-Host': 'imdb-search2.p.rapidapi.com'
-    }
-  };
-
-
-  //PROMISES
-
-  // return fetch(url, options)
-  //   .then(response => {
-  //     return response.json()
-  //   })
-  //   .catch(error => console.error(error));
-
-
-  //TRY/CATCH WITH AWAIT
-
-  // const response = await fetch(url, options);
-  //   try {
-  //     const response = await fetch(url, options);
-  //     const result = await response.json();
-  //     console.log(result)
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
+const generatePropmt = function(i) {
+  if (i.legth === 0 || i.trim().length === 0) {
+    return `Say 'uncategorized'`;
+  }
+  return `Categorize ${i} as a book, movie, television, restaurant or product.`;
 };
 
-// CALL FUNCTION
-// console.log(toWatch('bambi '));
+// Use openai to categorize user input
 
-
-///////////////////////////////////////////////////////////////////////////
-
-
-
-const toEat = function(input) {
-  client.search({
-    term: `${input}`,
-    location: "Victoria, BC"
-  }).then(response => {
-    console.log(response.jsonBody.businesses[0].name);
-  }).catch(error => {
-    console.log(error);
-  });
+const categorizeItem = async function(item) {
+  let resultTrim = [];
+  const url = `https://api.openai.com/v1/completions`;
+  const options = {
+    body: JSON.stringify({"model": "text-davinci-003", "prompt": generatePropmt(item), "temperature": 0, "max_tokens": 7}),
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `${apiKey}`,
+    },
+  }
+  try {
+    const response = await fetch(url, options);
+    const results = await response.json();
+    const result = results.choices[0].text
+    resultTrim = result.trim().split(' ');
+    return resultTrim;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-// CALL FUNCTION
-// toEat("Ocean")
 
+// Use response from openai to assign a category id to user input
 
-///////////////////////////////////////////////////////////////////////////
-
-const toRead = async function(input) {
-  const url = `https://book-finder1.p.rapidapi.com/api/search?title=${input}`;
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '02f1e4e396mshf4d0b21c733446bp12fe4cjsn159bedb15bbe',
-      'X-RapidAPI-Host': 'book-finder1.p.rapidapi.com'
-    }
-  };
-
-  // PROMISES WITH CATCH
-  return fetch(url, options)
-    .then(response => {
-      return response.json();
+const getCatId = function(input) {
+  categorizeItem(input)
+    .then((result) => {
+      console.log(result)
+      if(result.includes("Movie" || "Television")) {
+        console.log("To Watch");
+      } else if(result.includes("Restaurant")) {
+        console.log("To Eat");
+      } else if(result.includes("Book")) {
+        console.log("To Read");
+      } else if(result.includes("Product")) {
+        console.log("To Buy");
+      } else {
+        console.log("Uncategorized");
+      }
     })
-    .catch(error => console.error(error));
-
-
-  //TRY/CATCH WITH AWAIT
-
-  // try {
-  //   const response = await fetch(url, options);
-  //   const result = await response.json();
-  //   const details = await result.results[0];
-  //   const title = await details.title
-  //   console.log(title);
-  // } catch (error) {
-  //   console.error(error);
-  // }
-
+  .catch((error) => console.error(error));
 };
 
-// CALL FUNCTION
-// toRead('harry potter and the half blood prince')
+getCatId("Harry Potter")
 
 
-///////////////////////////////////////////////////////////////////////////
-
-// const toBuy = function(input) {
-
-// };
 
 
-// module.exports = { toWatch, toEat, toRead, toBuy}
+
+
+module.exports = { getCatId };
+
+
+
+
+
+
+
+
+
+
+
+
+
