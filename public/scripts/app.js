17572f22095dc7ec4e342372d7b8642c7663b030
Katherine Struthers <ktstruthers@gmail.com>
2023-07-20 14:38:12 -0500

    list functions w/ comments that need to be written

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,31 @@
-// Client facing scripts here
+/* eslint-disable no-undef */
+
+$(document).ready(function() {
+
+  /**
+   * Create individual funsie element (checkbox, name, & select category)
+   * @param {object} funsie - Object containing info about individual funsie.
+   * @return {object} $funsie - jQuery object containing HTML to create element.
+  */
+  const createFunsieElement = function(funsie) {
+    let $funsie = $(``);
+
+    return $funsie;
+  };
+
+  /**
+   * Loop through all funsies to create a list of all elements
+   * @param {array} funsies - Array of funsie objects.
+  */
+  const renderFunsies = function(funsies) {
+
+  };
+
+  /** Load funsies after successful AJAX request */
+  const loadFunsies = function() {
+
+  };
+
+  /** Initial funsies on page-load */
+  loadFunsies();
+});
