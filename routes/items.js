37bff4c267dae05ffb120f9d9e2067a4c28f5236
Carmelia Shito <carmshito@hotmail.com>
2023-07-20 15:12:24 -0400

    add items route

---
+++ routes/items.js
@@ -0,0 +1,26 @@
+/*
+ * All routes for items are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /api/users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router = express.Router();
+const itemsQueries = require('../db/queries/get-all-items');
+
+// get all items
+router.get("/items", (req, res) => {
+  itemsQueries
+    .getAllItems(req.query)
+    .then(items => {
+      console.log(items);
+    })
+    .catch((e) => {
+      console.log(e);
+      res.send(e);
+    });
+});
+
+
+module.exports = router;
