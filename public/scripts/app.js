7650eb16c852f4ce80a6854986df8335f0f6cd41
JacquelineMG <j.gilchrist@outlook.com>
2023-07-21 17:22:23 -0700

    write prelim function for changing drop box colours

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,17 @@
 // Client facing scripts here
+
+
+$(document).ready(function() {
+  for (let i = 1; i < 10; i++) {
+    let count = i
+    let categories = `${count}-categories`
+    const category = ($(`#${categories}`).val())
+    $(`#${categories}`).removeClass().addClass(`colour${category}`);
+
+     $(`#${categories}`).on( "change", function() {
+      cat = ($(`#${categories}`).val())
+
+      $(`#${categories}`).removeClass().addClass(`colour${cat}`);
+    });
+  }
+});
