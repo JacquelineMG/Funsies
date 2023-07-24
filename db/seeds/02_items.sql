6a0454b068d6ff4c1632198505a5224dd0c015a1
JacquelineMG <j.gilchrist@outlook.com>
2023-07-24 10:12:00 -0700

    add more items to seeds

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -5,7 +5,7 @@ VALUES (4, 'Shoes', false);
 INSERT INTO items (category_id, title, is_done)
 VALUES (2, 'Atomic Habits', false);
 INSERT INTO items (category_id, title, is_done)
-VALUES (3, 'Wendy`s', false);
+VALUES (3, 'Wendys', false);
 
 INSERT INTO items (category_id, title, is_done)
 VALUES (4, 'Shampoo', false);
