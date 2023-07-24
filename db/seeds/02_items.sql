0e3308ed71cfde582ae2d4cffb0ed958724d79ae
JacquelineMG <j.gilchrist@outlook.com>
2023-07-24 10:10:16 -0700

    add more items to seeds

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -5,4 +5,50 @@ VALUES (4, 'Shoes', false);
 INSERT INTO items (category_id, title, is_done)
 VALUES (2, 'Atomic Habits', false);
 INSERT INTO items (category_id, title, is_done)
-VALUES (3, 'Wendys', false);
+VALUES (3, 'Wendy`s', false);
+
+INSERT INTO items (category_id, title, is_done)
+VALUES (4, 'Shampoo', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (2, 'Pride and Prejudice', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (3, 'Blue Water Cafe', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (4, 'Rain jacket', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (2, 'Lord of the Rings', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (1, 'Star Wars', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (3, 'McDonald`s', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (1, 'Parasite', false);
+
+
+INSERT INTO items (category_id, title, is_done)
+VALUES (4, 'Sun hat', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (4, 'Sunglasses', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (2, 'A Tale of Two Cities', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (1, 'Spirited Away', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (4, 'Beach towel', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (1, 'Casablanca', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (3, 'Virtuous Pie', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (2, 'Atonement', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (2, 'The Great Gatsby', false);
+VALUES (1, 'Oppenheimer', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (1, 'Greta Gerwig`s Barbie', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (2, 'Frankenstein', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (4, 'Patio chairs', false);
+INSERT INTO items (category_id, title, is_done)
+VALUES (3, 'Baskin-Robins', false);
