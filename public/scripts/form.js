ae5dc35740c22c2a6126d00fa5f67599f4dce777
Katherine Struthers <ktstruthers@gmail.com>
2023-07-24 22:06:28 -0500

    add slide-up functionality to +add funsie

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -2,20 +2,30 @@
 /* eslint-disable no-undef */
 
 $(document).ready(function() {
+  let formDown = false;
+
   $(document).on('click', "#add-funsie", (event) => {
     event.preventDefault();
-    
     const form = document.getElementById('new-funsie-form');
-    
-    // slide form down when "add funsie" is clicked
-    $("#new-funsie-form")
-    .slideDown()
-    .show();
-    form.style.display = 'flex';
+
+    // slide form down when "add funsie" is initially clicked
+    // slide form up when "add funsie" is clicked a second time
+    if (formDown) {
+      formDown = false;
+      $("#new-funsie-form")
+        .slideUp();
+    } else {
+      formDown = true;
+      $("#new-funsie-form")
+        .slideDown()
+        .show();
+        form.style.display = 'flex';
+    }
   });
 
   // slide form up when cancel button is clicked
   $(document).on('click', "#cancel-button", () => {
+    formDown = false;
     $("#new-funsie-form")
       .slideUp();
   });
