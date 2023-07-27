4fa9353585afa958bc5e27a2f51b8260f52d31c6
Katherine Struthers <ktstruthers@gmail.com>
2023-07-27 13:15:42 -0500

    adjust seeds for presentation

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -1,55 +1,60 @@
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (1, 'Titanic', '2023-07-01T14:47:00.741Z', false);
+VALUES (1, 'Spirited Away', '2023-07-04T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (4, 'Shoes', '2023-07-02T14:47:00.741Z', false);
+VALUES (4, 'Patio chairs', '2023-07-03T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (2, 'Atomic Habits', '2023-07-03T14:47:00.741Z', false);
+VALUES (3, 'Pho No. 1', '2023-07-02T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (3, 'Wendys', '2023-07-04T14:47:00.741Z', false);
+VALUES (2, 'The Clean Coder', '2023-07-01T14:47:00.741Z', false);
 
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (4, 'Shampoo', '2023-07-05T14:47:00.741Z', false);
+VALUES (1, 'Titanic', '2023-06-01T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (2, 'Pride and Prejudice', '2023-07-06T14:47:00.741Z', false);
+VALUES (4, 'Shoes', '2023-06-02T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (3, 'Blue Water Cafe', '2023-07-07T14:47:00.741Z', false);
+VALUES (2, 'Atomic Habits', '2023-06-03T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (4, 'Rain jacket', '2023-07-08T14:47:00.741Z', false);
+VALUES (3, 'Wendys', '2023-06-04T14:47:00.741Z', false);
+
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (2, 'Lord of the Rings', '2023-07-09T14:47:00.741Z', false);
+VALUES (4, 'Shampo', '2023-06-05T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (1, 'Star Wars', '2023-07-10T14:47:00.741Z', false);
+VALUES (2, 'Pride and Prejudice', '2023-06-06T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (3, 'McDonald`s', '2023-07-11T14:47:00.741Z', false);
+VALUES (3, 'Blue Water Cafe', '2023-06-06T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (1, 'Parasite', '2023-07-12T14:47:00.741Z', false);
+VALUES (4, 'Rain jacket', '2023-06-08T14:47:00.741Z', false);
+INSERT INTO items (category_id, title, created_date, is_done)
+VALUES (2, 'Lord of the Rings', '2023-06-09T14:47:00.741Z', false);
+INSERT INTO items (category_id, title, created_date, is_done)
+VALUES (1, 'Star Wars', '2023-06-10T14:47:00.741Z', false);
+INSERT INTO items (category_id, title, created_date, is_done)
+VALUES (3, 'McDonald`s', '2023-06-11T14:47:00.741Z', false);
+INSERT INTO items (category_id, title, created_date, is_done)
+VALUES (1, 'Parasite', '2023-06-12T14:47:00.741Z', false);
 
 
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (4, 'Sun hat', '2023-07-13T14:47:00.741Z', false);
+VALUES (4, 'Sun hat', '2023-06-13T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (4, 'Sunglasses', '2023-07-14T14:47:00.741Z', false);
+VALUES (4, 'Sunglasses', '2023-06-14T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (2, 'A Tale of Two Cities', '2023-07-15T14:47:00.741Z', false);
+VALUES (2, 'A Tale of Two Cities', '2023-06-15T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (1, 'Spirited Away', '2023-07-16T14:47:00.741Z', false);
+VALUES (4, 'Beach towel', '2023-06-17T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (4, 'Beach towel', '2023-07-17T14:47:00.741Z', false);
+VALUES (1, 'Casablanca', '2023-06-18T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (1, 'Casablanca', '2023-07-18T14:47:00.741Z', false);
+VALUES (3, 'Virtuous Pie', '2023-06-19T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (3, 'Virtuous Pie', '2023-07-19T14:47:00.741Z', false);
+VALUES (2, 'Atonement', '2023-06-20T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (2, 'Atonement', '2023-07-20T14:47:00.741Z', false);
+VALUES (2, 'The Great Gatsby', '2023-06-21T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (2, 'The Great Gatsby', '2023-07-21T14:47:00.741Z', false);
+VALUES (1, 'Oppenheimer', '2023-06-22T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (1, 'Oppenheimer', '2023-07-22T14:47:00.741Z', false);
+VALUES (1, 'Greta Gerwig`s Barbie', '2023-06-23T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (1, 'Greta Gerwig`s Barbie', '2023-07-23T14:47:00.741Z', false);
+VALUES (2, 'Frankenstein', '2023-06-24T14:47:00.741Z', false);
 INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (2, 'Frankenstein', '2023-07-24T14:47:00.741Z', false);
-INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (4, 'Patio chairs', '2023-07-25T14:47:00.741Z', false);
-INSERT INTO items (category_id, title, created_date, is_done)
-VALUES (3, 'Baskin-Robins', '2023-07-26T14:47:00.741Z', false);
+VALUES (3, 'Baskin-Robins', '2023-06-26T14:47:00.741Z', false);
